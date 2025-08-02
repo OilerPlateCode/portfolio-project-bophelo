f6bd3af3b28295c7b1a0b27724cde5f2962a22de
Mbuzulwane Ginindza <mpilo.dlamini@yahoo.com>
2025-08-02 19:15:56 +0200

    learn - using parth variable for find by 1

--- bophelo/src/main/java/com/oilerplatecode/bophelo/run/RunController.java
+++ bophelo/src/main/java/com/oilerplatecode/bophelo/run/RunController.java
@@ -1,6 +1,7 @@
 package com.oilerplatecode.bophelo.run;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +22,9 @@ public class RunController {
         return "Hello runner";
     }
 
-    @GetMapping("/1")
-    Run findById() {
-        return runRepository.findById(1);
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id) {
+        return runRepository.findById(id);
     }
 
     @GetMapping("")
