9138a81002e5dd27e93eea6feaa0f60059552cf6
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-08-14 13:02:12 +0200

    learn: reading runs form the database

--- src/main/java/com/oilerplatecode/bophelo/run/RunController.java
+++ src/main/java/com/oilerplatecode/bophelo/run/RunController.java
@@ -23,38 +23,38 @@ public class RunController {
         return "Hello runner";
     }
 
-    @GetMapping("/{id}")
-    Run findById(@PathVariable Integer id) {
-        Optional<Run> run = runRepository.findById(id);
-
-        if(run.isEmpty()) {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            throw new RunNotFoundException();
-        }
-        return run.get(); // we had this get in the repo when it was not options
-    }
+//    @GetMapping("/{id}")
+//    Run findById(@PathVariable Integer id) {
+//        Optional<Run> run = runRepository.findById(id);
+//
+//        if(run.isEmpty()) {
+////            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+//            throw new RunNotFoundException();
+//        }
+//        return run.get(); // we had this get in the repo when it was not options
+//    }
 
     @GetMapping("")
     List<Run> findAll() {
         return runRepository.findAll();
     }
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("")
-    void create(@Valid @RequestBody Run run) {
-        runRepository.create(run);
-    }
-
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id) {
-        runRepository.update(run, id);
-    }
-
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{id}")
-    void delete(@PathVariable Integer id) {
-        runRepository.delete(id);
-    }
+//
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @PostMapping("")
+//    void create(@Valid @RequestBody Run run) {
+//        runRepository.create(run);
+//    }
+//
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    @PutMapping("/{id}")
+//    void update(@RequestBody Run run, @PathVariable Integer id) {
+//        runRepository.update(run, id);
+//    }
+//
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    @DeleteMapping("/{id}")
+//    void delete(@PathVariable Integer id) {
+//        runRepository.delete(id);
+//    }
 }
 
--- src/main/java/com/oilerplatecode/bophelo/run/RunRepository.java
+++ src/main/java/com/oilerplatecode/bophelo/run/RunRepository.java
@@ -1,6 +1,9 @@
 package com.oilerplatecode.bophelo.run;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -12,38 +15,27 @@ import java.util.Optional;
 
 @Repository
 public class RunRepository {
-    private List<Run> runs = new ArrayList<>();
+    private final Logger logger = LoggerFactory.getLogger(RunRepository.class);
+    private final JdbcClient jdbcClient;
+
+    public RunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
 
     List<Run> findAll() {
-        return runs;
+        return jdbcClient.sql("select * from run").query(Run.class).list();
     }
 
-    Optional<Run> findById(Integer id) {
-        return runs.stream().filter(run -> run.id() == id).findFirst();
-    }
-
-    void create(Run run) {
-        runs.add(run);
-    }
-
-    void update(Run run, Integer id) {
-        Optional<Run> existingRun = findById(id);
-        if(existingRun.isPresent()) {
-            runs.set(runs.indexOf(existingRun.get()), run);
-        }
-    }
-
-    void delete(Integer id) {
-        runs.removeIf(run -> run.id().equals(id));
-    }
-
-    @PostConstruct
-    private void init() {
-        runs.add(
-                new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5000, Location.OUTDOOR)
-        );
-        runs.add(
-                new Run(2, "Second Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.MINUTES), 3000, Location.INDOOR)
-        );
-    }
+//    Optional<Run> findById(Integer id) {
+//    }
+//
+//    void create(Run run) {
+//    }
+//
+//    void update(Run run, Integer id) {
+//
+//    }
+//
+//    void delete(Integer id) {
+//    }
 }
