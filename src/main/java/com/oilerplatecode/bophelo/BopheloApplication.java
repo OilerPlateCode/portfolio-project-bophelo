53dc92df3a210e98e58921a20f07335a00e125d1
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-09-04 20:30:46 +0200

    REFACTOR - removing files i no longer need and using sping data starter

--- src/main/java/com/oilerplatecode/bophelo/BopheloApplication.java
+++ src/main/java/com/oilerplatecode/bophelo/BopheloApplication.java
@@ -1,17 +1,12 @@
 package com.oilerplatecode.bophelo;
 
-import com.oilerplatecode.bophelo.run.RunRepository;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.oilerplatecode.bophelo.run.Location;
-import com.oilerplatecode.bophelo.run.Run;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class BopheloApplication {
@@ -20,16 +15,6 @@ public class BopheloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BopheloApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner runner(RunRepository runRepository) {
-		return args -> {
-			Run run = new Run(11, "Test Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5000, Location.INDOOR);
-			log.info("Running " + run);
-			log.info("something changed");
-			runRepository.create(run);
-		};
 	}
 
 }
--- src/main/java/com/oilerplatecode/bophelo/run/RunRepository.java
+++ src/main/java/com/oilerplatecode/bophelo/run/RunRepository.java
@@ -1,75 +1,11 @@
 package com.oilerplatecode.bophelo.run;
 
-import jakarta.annotation.PostConstruct;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.Assert;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+
 
 @Repository
-public class RunRepository {
-    private final Logger logger = LoggerFactory.getLogger(RunRepository.class);
-    private final JdbcClient jdbcClient;
+public interface RunRepository extends ListCrudRepository<Run, Integer> {
 
-    public RunRepository(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;
-    }
-
-    List<Run> findAll() {
-        return jdbcClient.sql("select * from run").query(Run.class).list();
-    }
-
-    Optional<Run> findById(Integer id) {
-        return jdbcClient.sql("SELECT id,title,started_on,completed_on,meters,location FROM Run WHERE id = :id")
-                .param("id", id)
-                .query(Run.class)
-                .optional();
-    }
-
-    public void create(Run run) {
-        var updated = jdbcClient.sql("INSERT INTO Run(id,title,started_on,completed_on,meters,location) values(?,?,?,?,?,?)")
-                .params(List.of(run.id(), run.title(), run.startedOn(), run.completedOn(), run.meters(), run.location().toString()))
-                .update();
-
-        Assert.state(updated == 1, "Failed to create run " + run.title());
-    }
-
-    void update(Run run, Integer id) {
-        var updated = jdbcClient.sql("update run set title = ?, started_on = ?, completed_on = ?, meters = ?, location = ? where id = ?")
-                .params(List.of(run.title(), run.startedOn(), run.completedOn(), run.meters(), run.location().toString(), id))
-                .update();
-
-        Assert.state(updated == 1, "Failed to update run " + run.title());
-    }
-
-    void delete(Integer id) {
-        var updated = jdbcClient.sql("delete from run where id = :id")
-                .param("id", id)
-                .update();
-
-        Assert.state(updated == 1, "Failed to delete run " + id);
-    }
-
-    public int count() {
-        return jdbcClient.sql("select * from run").query().listOfRows().size();
-    }
-
-    public void saveAll(List<Run> runs) {
-        runs.stream().forEach(this::create);
-    }
-
-    public List<Run> findByLocation(String location) {
-        return jdbcClient.sql("select * from run where location = :location")
-                .param("location", location)
-                .query(Run.class)
-                .list();
-    }
 }
