1c3749135d5313299495c86ec6cd6d18091dc263
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-09-10 18:51:08 +0200

    unit test - testing other methods in run controller

--- src/test/java/com/oilerplatecode/bophelo/run/RunControllerTest.java
+++ src/test/java/com/oilerplatecode/bophelo/run/RunControllerTest.java
@@ -3,8 +3,10 @@ package com.oilerplatecode.bophelo.run;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -13,17 +15,20 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.hamcrest.Matchers.is;
+
 
 @WebMvcTest(RunController.class)
 class RunControllerTest {
 
     @Autowired
-    MockMvc mockMvc;
+    MockMvc mvc;
 
     @Autowired
     ObjectMapper objectMapper;
@@ -49,8 +54,61 @@ class RunControllerTest {
     @Test
     void shouldFindAllRuns() throws Exception {
         when(runRepository.findAll()).thenReturn(runs);
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/runs"))
+        mvc.perform(MockMvcRequestBuilders.get("/api/runs"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.size()", is(runs.size())));
     }
+
+//    @Test
+//    void shouldCreateNewRun() throws Exception {
+//        var run = new Run(1,"test", LocalDateTime.now(),LocalDateTime.now(),1, Location.INDOOR, null);
+//        mvc.perform(MockMvcRequestBuilders.post("/api/runs"))
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectMapper.writeValueAsString(run));
+//    }
+
+    @Test
+    void shouldFindOneRun() throws Exception {
+        Run run = runs.get(0);
+        when(runRepository.findById(ArgumentMatchers.anyInt())).thenReturn(Optional.of(run));
+        mvc.perform(get("/api/runs/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(run.id())))
+                .andExpect(jsonPath("$.title", is(run.title())))
+                .andExpect(jsonPath("$.miles", is(run.meters())))
+                .andExpect(jsonPath("$.location", is(run.location().toString())));
+    }
+
+    @Test
+    void shouldReturnNotFoundWithInvalidId() throws Exception {
+        mvc.perform(get("/api/runs/99"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldCreateNewRun() throws Exception {
+        var run = new Run(3,"test", LocalDateTime.now(),LocalDateTime.now(),1, Location.INDOOR, null);
+        mvc.perform(post("/api/runs")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(run))
+                )
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void shouldUpdateRun() throws Exception {
+        var run = new Run(2,"test", LocalDateTime.now(),LocalDateTime.now(),1, Location.INDOOR, null);
+        mvc.perform(put("/api/runs/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(run))
+                )
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void shouldDeleteRun() throws Exception {
+        mvc.perform(delete("/api/runs/1"))
+                .andExpect(status().isNoContent());
+    }
+
 }
