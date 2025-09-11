d1da553927d98888dedcfbf74f531ec271392525
OilerPlateCode <mbuzulwane.ginindza@gmail.com>
2025-09-11 19:33:27 +0200

    int test - saving WIP

---
+++ src/test/java/com/oilerplatecode/bophelo/run/RunControllerIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.oilerplatecode.bophelo.run;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RunControllerIntegrationTest {
+@LocalServerPort
+    private int randomServerPort;
+RestClient restClient;
+
+@BeforeEach
+    void setUp() {
+    restClient = RestClient.create("https://localhost:" + randomServerPort);
+}
+
+    @Test
+    void shouldFindAllRuns() {
+List<Run> runs = restClient.get()
+        .uri("/api/runs")
+        .retrieve()
+        .body(new ParameterizedTypeReference<List<Run>>() {
+        });
+assertEquals(10, runs.size());
+    }
+}
